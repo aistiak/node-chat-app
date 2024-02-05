41c80c70378fe215dfe8d95af89845b3a8c27c48
aistiak <aristiak1994@gmail.com>
2024-02-05 23:37:36 +0600

    update

---
+++ server/main.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors") ;
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketio(server,{
+    path : '/',
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+});
+
+
+app.get('/', (req, res) => {
+
+    return res.sendStatus(200);
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle events from the client
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+
+        // Broadcast the message to all connected clients
+        io.emit('chat message', msg);
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const port = 3010;
+server.listen(port, () => {
+    console.log(` -- server started on port ${port} --`)
+})
